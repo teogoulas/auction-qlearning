21359bbaa1905a4c29e426489c4abd466ef55b36
Thodoris Goulas <t.goulas@paymentcomponents.com>
2021-02-24 00:56:10 +0200

    Added tests

---
+++ tests/test_environment.py
@@ -0,0 +1,18 @@
+from environment.action import Action
+from environment.environment import Environment
+
+from utils.constants import NUM_EPISODES
+
+env = Environment()
+for episode in range(0, NUM_EPISODES):
+    state = env.reset()
+    done = False
+    score = 0
+
+    while not done:
+
+        action = Action(0, env.action_space.sample(), env.action_space.sample())
+        n_state, reward, done, info = env.step(action)
+        score += reward
+
+    print(f"Episode: {episode} Score: {score}")
